d39ca5596f7fd06546fd304ba99c21ef3cce7c72
Verma <niranjan_verma@optum.com>
2020-07-09 18:17:44 +0530

    angular-widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Altero-Dashboard';
+  mySlideImages = [1,2,3].map((i)=> `https://picsum.photos/640/480?image=${i}`);
+  myCarouselImages =[1,2,3,4,5,6].map((i)=>`https://picsum.photos/640/480?image=${i}`);
+  mySlideOptions={items: 1, dots: true, nav: false};
+  myCarouselOptions={items: 3, dots: true, nav: true};
 }
+
